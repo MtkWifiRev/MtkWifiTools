1bb9da7d2de335bd1c8cfde5661e9ae925be5c33
Edoardo Mantovani <168632450+PurpleVortex@users.noreply.github.com>
2025-10-15 12:19:23 +0200

    Update nds32_code.c

--- barepatcher/code_output/nds32_code.c
+++ barepatcher/code_output/nds32_code.c
@@ -1,13 +1,18 @@
-// patch at 0xe0271db8
-__attribute__((aligned(4))) int _start(int param_1){
-        void (*ptr)(char *str) = (void *)0x92b0a0;
-	void (*ptr1)(int perm) = (void *)0x92f272;
-	ptr1(4);
-	ptr((char *)0x2021bd0);
-	ptr((char *)0x2021bd0);
-	ptr((char *)0x2021bd0);
-	ptr1(4);
-	ptr((char *)0x2021bd0);
-	return 0;
-}
+// to patch at 00071d0c
 
+#include "inc/include.h"
+
+__attribute__((nacked)) __attribute__((aligned(4))) void _start(void *ptr){
+	//void (*addr)(void) = (void *)0x00805e56;
+	//addr();
+
+	uint32_t *src 		= (uint32_t *)0x068400fc; // last addr: 0x008433c8;
+	uint32_t *dst 		= (uint32_t *)0xe020c338;
+	uint16_t  sl 		= 250;
+	for(unsigned char i = 0; i < sl; i++){
+		//__asm__ volatile("" ::: "memory");
+		dst[i] 	= src[i];
+		//__asm__ volatile("" ::: "memory");
+	}
+}
+// then dump memory at '0x7f82e'
