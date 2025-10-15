aad91bac9cefe8085e4e73caa8b6cd36e60fddce
Edoardo Mantovani <168632450+PurpleVortex@users.noreply.github.com>
2025-10-15 12:20:33 +0200

    Add files via upload

---
+++ barepatcher/code_output/inc/include.h
@@ -0,0 +1,11 @@
+#ifndef __NDS32_CODE_INCLUDER__
+#define __NDS32_CODE_INCLUDER__
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define MT7922_INJECT_LEN	500
+#define MT7922_DEST_ADDR 	0xe020f82e
+#define MT7922_SRC_ADDR  	0x00800190
+
+#endif
